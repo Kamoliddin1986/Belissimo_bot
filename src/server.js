aecc553a1c86c875b26a677abdf62112bf49c017
Kamoliddin <kamoliddinabduraimov@gmail.com>
2023-02-26 17:17:02 +0500

    created email sending serviice

--- src/server.js
+++ src/server.js
@@ -1,11 +1,64 @@
 import {Markup, Telegraf} from 'telegraf'
 import dotenv from 'dotenv'
+
+import nodemailer from 'nodemailer'
+
 dotenv.config()
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS
+    }
+})
+
+
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+function isEmail(emailAdress){
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (emailAdress.match(regex)) 
+    return true; 
+
+   else 
+    return false; 
+}
 bot.start( async (ctx) => {
-    ctx.reply('Start')
+
+    await ctx.reply("Emailni jo'nating")
+
+})
+
+bot.on("text", async ctx => {
+
+    let Email = await isEmail(ctx.update.message.text)
+    console.log(Email);
+    if(Email){
+
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: ctx.update.message.text,
+            subject: "Accept your email",
+            text: 'something....'
+        }
+
+        transporter.sendMail(mailOptions, function(error,info){
+            if(error){
+                console.log(error)
+            }else{
+                console.log("Email sent>>>>", info.response);
+            }
+        })
+        ctx.reply('email ok')
+    }else{
+        ctx.reply('email nok')
+    }
+
+
 })
 
 bot.launch(console.log('RUNNING'))
